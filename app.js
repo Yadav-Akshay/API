d8cce8db2516d63b4274df8d2c33606c8ed0b521
Akshay-Yadav <akshayyadav3300@gmail.com>
2022-11-05 18:01:47 +0530

    format document

--- app.js
+++ app.js
@@ -38,15 +38,15 @@ app.use('/city', cityR);
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   //res.locals.message = err.message;
-res.locals.message = err.message;
+  res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
