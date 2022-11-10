5610c0a7857733d004af2d0cb9b9c4deec916813
Akshay-Yadav <akshayyadav3300@gmail.com>
2022-11-10 12:17:31 +0530

    add cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -27,6 +28,7 @@ var t = Handlebars.compile($('#t').html());
 $('body').append(t());
 */
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
